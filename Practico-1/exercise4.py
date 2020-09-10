75ed4b0e684b159d5af0c4b720024ec26c08723e
CarusoX <carusox@protonmail.com>
2020-09-10 16:10:59 -0700

    Some changes

--- Practico-1/exercise4.py
+++ Practico-1/exercise4.py
@@ -7,7 +7,8 @@ def is_valid(s):
     return all(32 <= ord(c) <= 126 for c in s) and len(opened) == 1 and len(closed) == 1 and opened[0] < closed[0]
 
 def do_otp(message, key):
-    k = "".join([key[i % len(key)] for i in len(message)]) # Complete the key
+    print(message, key)
+    k = "".join([key[i % len(key)] for i in range(len(message))]) # Complete the key
     x = int(message, 16) ^ int(k, 16)
     return '%x' % x
 
