e8196bf51bd49547407f864dfb1d5347be42d014
octaviotastico <octaviopercivaldi2@gmail.com>
2020-10-22 09:28:30 -0300

    Working on Pipe

---
+++ Entregable-4/exercise1/test.c
@@ -0,0 +1,22 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+int main(void) {
+  int i = 0; // iStack152 -> Posicion actual
+  char c; // cVar1 -> Caracter en posicion actual
+  unsigned int r; // uVar4 -> Random number
+  char buffer [128]; // acStack148 -> Input nuestro
+  srand(0x08040866c); // Semilla del rand, (main address)
+  fgets(buffer, 128, stdin); // Mismo fgets, 128=0x80
+
+  while ((buffer[i] != '\0' && (buffer[i] != '\n'))) {
+    c = buffer[i];
+    r = rand();
+    r = (r & 0xFF) ^ (int)c;
+    printf("%02x", r); // Necesito al menos 2 digitos
+    i = i + 1;
+  }
+  putchar(10);
+
+  return 0;
+}
